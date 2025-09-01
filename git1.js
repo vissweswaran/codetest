480bf42a8312d61ba460b77c7ca193268b329701
Vishwa <vissweswaran27@gmail.com>
2025-09-02 00:12:20 +0530

    Initial commit

---
+++ git1.js
@@ -0,0 +1,30 @@
+const { Before, After, AfterStep } = require('@cucumber/cucumber');
+const { LoginPage } = require('../Newage/pages/loginpage');
+
+Before(async function () {
+  await this.launchBrowser();
+  // this.loginPage = new LoginPage(this.page, this.config);
+});
+
+// AfterStep(async function ({ result }) {
+//   if (result?.status === 'FAILED') {
+//     const screenshot = await this.page.screenshot({ fullPage: true });
+//     await this.attach(screenshot, 'image/png'); // attaches to report
+//   }
+// });
+
+AfterStep(async function (scenario) {
+  if (scenario.result.status === 'FAILED') {
+    if (this.page) {
+      const screenshot = await this.page.screenshot();
+      this.attach(screenshot, 'image/png');
+    }
+    if (scenario.result.exception) {
+      this.attach(scenario.result.exception.stack || scenario.result.exception.message);
+    }
+  }
+});
+
+After(async function () {
+  await this.closeBrowser(); // no screenshot needed here
+});
---
+++ git2.js
@@ -0,0 +1,38 @@
+const { setWorldConstructor, World, setDefaultTimeout  } = require('@cucumber/cucumber');
+// const playwright = require('playwright');
+const { chromium} = require('@playwright/test');
+const path = require('path');
+// const { LoginPage } = require('../Newage/pages/loginpage');
+/** @typedef {import('../Newage/pages/loginpage')} LoginPage */
+
+// Load config based on ENV
+const env = process.env.ENV || 'qa';
+const config = require(path.join(__dirname, '../Newage/Configs', `${env}.js`));
+
+// Set Cucumber step default timeout to 30 seconds
+setDefaultTimeout(30 * 1000);
+
+class CustomWorld extends World {
+  
+  constructor(options) {
+    super(options); // ✅ gets attach() and parameters from Cucumber
+    this.config = config;
+  }
+
+  async launchBrowser() {
+    this.browser = await chromium.launch({ headless: this.config.headless,
+       args: ['--start-maximized'],  
+     });
+    this.context = await this.browser.newContext({viewport: null, });
+    this.page = await this.context.newPage();
+    
+  }
+
+  async closeBrowser() {
+    await this.browser.close();
+  }
+}
+
+setWorldConstructor(CustomWorld);
+
+
